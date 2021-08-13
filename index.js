ff72c2ab266ca3242e06317d865b6b52a2ac6298
fellipe lorram <fellipelorram@gmail.com>
2021-08-13 09:09:53 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,52 @@
+const toggleSideBar = document.querySelector('.top_bar_toggle');
+const main = document.querySelector('.main');
+const navigation = document.querySelector('.navigation');
+toggleSideBar.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+});
+
+// ACCORDION
+
+
+// MOVER ADD PACIENTE
+
+const open_with = document.querySelector('.open_with');
+const open_with_left_side = document.querySelector('.open_with_left_side');
+
+open_with.addEventListener('click', e => {
+    console.log(e.target.parentElement)
+    e.target.parentElement.style.left = "calc(100vw - 25%)";
+    e.target.style.display = "none";
+    open_with_left_side.style.display = "block";
+});
+
+open_with_left_side.addEventListener('click', e => {
+    console.log(e.target.parentElement)
+    e.target.parentElement.style.left = "35px";
+    e.target.style.display = "none";
+    open_with.style.display = "block";
+});
+
+let prevPageYOfsset = window.pageYOffset;
+window.onscroll = () => {
+    let currentPageYOfsset = window.pageYOffset;
+
+    if (prevPageYOfsset > currentPageYOfsset) {
+        document.querySelector(".main_top_bar").style.top = "0";
+        document.querySelector(".navigation_mobile").style.bottom = "0";
+    }
+    else {
+        document.querySelector(".main_top_bar").style.top = "-100px";
+        document.querySelector(".navigation_mobile").style.bottom = "-100px";
+    }
+    prevPageYOfsset = currentPageYOfsset;
+
+    if (document.querySelector(".main_top_bar").style.top == "-100px") {
+        setTimeout(() => {
+            document.querySelector(".main_top_bar").style.top = "0";
+            document.querySelector(".navigation_mobile").style.bottom = "0";
+        }, 1563);
+
+    }
+}
